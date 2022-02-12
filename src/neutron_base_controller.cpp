1236826801c411f21f7c9fb733a3970990989e99
selimkarahan <44104077+selimkarahan@users.noreply.github.com>
2022-02-11 17:51:25 -0800

    Update neutron_base_controller.cpp
    http://docs.ros.org/en/jade/api/pid/html/controller_8cpp_source.html

--- src/neutron_base_controller.cpp
+++ src/neutron_base_controller.cpp
@@ -4,14 +4,41 @@
 double angular_v  = 0;
 
 void trajectoryReceived(const geometry_msgs::Twist &msg) {
-	//implement control system (PID) and control motors via VESC
- 	
-
+	//implement control system (PID) and control motors via VES
 
 	int motor_eff = 500;
 
 	int servo_eff = 500;
+	
+	if ( !((Kp<=0. && Ki<=0. && Kd<=0.) || (Kp>=0. && Ki>=0. && Kd>=0.)) ) // All 3 gains should have the same sign
+	{
+		ROS_WARN("All three gains (Kp, Ki, Kd) should have the same sign for stability.");
+	}
+	
+	plant_state = state_msg.data;
 
+	error.at(2) = error.at(1);
+	error.at(1) = error.at(0);
+	error.at(0) = setpoint - plant_state; // Current error goes to slot 0
+
+	// calculate delta_t
+	if (!prev_time.isZero()) // Not first time through the program  
+	{
+		delta_t = ros::Time::now() - prev_time;
+		prev_time = ros::Time::now();
+		if (0 == delta_t.toSec())
+	{
+		ROS_ERROR("delta_t is 0, skipping this loop. Possible overloaded cpu at time: %f", ros::Time::now().toSec());
+	       return;
+		}
+	}
+	else
+	{
+		ROS_INFO("prev_time is 0, doing nothing");
+		prev_time = ros::Time::now();
+		return;
+	}
+	
 
 	ros::NodeHandle n;
 	//TO DO: This shit below dont work for some reason, I think it has to do with <int> part but idk
@@ -27,6 +54,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "neutron_base_controller");
 
 	ros::NodeHandle n;
+	ros::NodeHandle node_priv("~");
 
 	// parameters 
 	node_priv.param<double>("Kp", Kp, 1.0);
