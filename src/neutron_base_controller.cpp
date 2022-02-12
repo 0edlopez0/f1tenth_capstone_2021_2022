04fe356228e823c928c7baf264e64287fbe2ba92
selimkarahan <44104077+selimkarahan@users.noreply.github.com>
2022-02-11 17:44:15 -0800

    Update neutron_base_controller.cpp

--- src/neutron_base_controller.cpp
+++ src/neutron_base_controller.cpp
@@ -7,9 +7,6 @@ void trajectoryReceived(const geometry_msgs::Twist &msg) {
 	//implement control system (PID) and control motors via VESC
  	
 
-	
-
-
 
 	int motor_eff = 500;
 
@@ -31,6 +28,26 @@ int main(int argc, char **argv) {
 
 	ros::NodeHandle n;
 
+	// parameters 
+	node_priv.param<double>("Kp", Kp, 1.0);
+	node_priv.param<double>("Ki", Ki, 0.0);
+	node_priv.param<double>("Kd", Kd, 0.0);
+	node_priv.param<double>("upper_limit", upper_limit, 1000.0);
+	node_priv.param<double>("lower_limit", lower_limit, -1000.0);
+	node_priv.param<double>("windup_limit", windup_limit, 1000.0);
+	node_priv.param<double>("cutoff_frequency", cutoff_frequency, -1.0);
+	node_priv.param<std::string>("topic_from_controller", topic_from_controller, "control_effort");
+	node_priv.param<std::string>("topic_from_plant", topic_from_plant, "state");
+	node_priv.param<std::string>("setpoint_topic", setpoint_topic, "setpoint");
+	node_priv.param<std::string>("pid_enable_topic", pid_enable_topic, "pid_enable");
+	node_priv.param<double>("max_loop_frequency", max_loop_frequency, 1.0);
+	node_priv.param<double>("min_loop_frequency", min_loop_frequency, 1000.0);
+
+	// Two parameters to allow for error calculation with discontinous value
+	node_priv.param<bool>("angle_error", angle_error, false);
+	node_priv.param<double>("angle_wrap", angle_wrap, 2.0*3.14159);
+	
+	
 	ros::Subscriber sub = n.subscribe("cmd_vel", 1000, trajectoryReceived);
 
 	ros::spin();
