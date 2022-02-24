f29e4f2ebf436acf29a3f672f3f6384566e2712a
selimkarahan <44104077+selimkarahan@users.noreply.github.com>
2022-02-23 16:47:27 -0800

    Update neutron_base_controller.cpp
    http://docs.ros.org/en/jade/api/pid/html/controller_8cpp_source.html

--- src/neutron_base_controller.cpp
+++ src/neutron_base_controller.cpp
@@ -3,8 +3,8 @@
 
 double angular_v  = 0;
 
-void trajectoryReceived(const geometry_msgs::Twist &msg) {
-	//implement control system (PID) and control motors via VES
+void trajectoryReceived(const geometry_msgs::Twist & state_vel) {
+	//implement control system (PID) and control motors via VESC
 
 	int motor_eff = 500;
 
@@ -15,11 +15,13 @@ void trajectoryReceived(const geometry_msgs::Twist &msg) {
 		ROS_WARN("All three gains (Kp, Ki, Kd) should have the same sign for stability.");
 	}
 	
-	plant_state = state_msg.data;
+	
+	motor_setpoint = cmd_vel;
+	motor_state = state_vel;
 
 	error.at(2) = error.at(1);
 	error.at(1) = error.at(0);
-	error.at(0) = setpoint - plant_state; // Current error goes to slot 0
+	error.at(0) = motor_setpoint - motor_state; // Current error goes to slot 0
 
 	// calculate delta_t
 	if (!prev_time.isZero()) // Not first time through the program  
@@ -39,14 +41,39 @@ void trajectoryReceived(const geometry_msgs::Twist &msg) {
 		return;
 	}
 	
-	double error = setpoint - input;
-	double errsum;
-	errsum += (error.at(0)*delta_t);
+	// integrate error
+	error_integral += error.at(0) * delta_t.toSec();
+	// Apply windup limit to limit the size of the integral term
+	if (error_integral > fabsf(windup_limit)){
+		error_integral = fabsf(windup_limit);
+	}
+	if (error_integral < -fabsf(windup_limit)){
+		error_integral = -fabsf(windup_limit);
+	}
+	
+	// take derivative of error
+	error_deriv.at(2) = error_deriv.at(1);
+	error_deriv.at(1) = error_deriv.at(0);
+	error_deriv.at(0) = (error.at(0)-error.at(1))/delta_t.toSec();
+	
+	// calculate control effort
+	proportional = Kp * error.at(0);
+	integral = Ki * error_integral;
+	derivative = Kd * error_deriv.at(0);
+	control_effort = proportional + integral + derivative;
 	
 	
+	// control effort limits
+	if (motor_control_effort > upper_limit){
+		motor_control_effort = upper_limit;
+	} else if (motor_control_effort < lower_limit){
+		motor_control_effort = lower_limit;
+	}
 	
-	output = Kp * error + Ki * errsum + kd * dErr;
-
+	// publish control effort
+	motor_control_effort_pub.publish(motor_control_effort);
+	
+	
 	ros::NodeHandle n;
 	//TO DO: This shit below dont work for some reason, I think it has to do with <int> part but idk
 	//ros::Publisher pub = n.advertise<int>("control_effort", 1000);
@@ -67,8 +94,8 @@ int main(int argc, char **argv) {
 	node_priv.param<double>("Kp", Kp, 1.0);
 	node_priv.param<double>("Ki", Ki, 0.0);
 	node_priv.param<double>("Kd", Kd, 0.0);
-	node_priv.param<double>("upper_limit", upper_limit, 1000.0);
-	node_priv.param<double>("lower_limit", lower_limit, -1000.0);
+	node_priv.param<double>("upper_limit", upper_limit, 100.0);
+	node_priv.param<double>("lower_limit", lower_limit, -100.0);
 	node_priv.param<double>("windup_limit", windup_limit, 1000.0);
 	node_priv.param<double>("cutoff_frequency", cutoff_frequency, -1.0);
 	node_priv.param<std::string>("topic_from_controller", topic_from_controller, "control_effort");
@@ -82,9 +109,13 @@ int main(int argc, char **argv) {
 	node_priv.param<bool>("angle_error", angle_error, false);
 	node_priv.param<double>("angle_wrap", angle_wrap, 2.0*3.14159);
 	
+	// instantiate publishers and subscribers
+	
+	ros::Publisher motor_control_effort_pub = n.advertise<int>("control_effort", 1000);
 	
 	ros::Subscriber sub = n.subscribe("cmd_vel", 1000, trajectoryReceived);
-
+	ros::Subscriber plant_state_sub = n.subscribe("state_vel",1000, trajectoryReceived);
+	
 	ros::spin();
 
 	return 0;
